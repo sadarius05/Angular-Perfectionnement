20225f58408bb1ee78edcd3506123fdf50db3b33
Souliman <souleymane.sada@gmail.com>
2023-02-17 20:29:06 +0000

    WIP some functions with triggers for comments animation'sequence(),stagger()...'

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,4 +1,15 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
@@ -7,80 +18,127 @@ import { Comment } from 'src/app/core/models/comment.model';
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss'],
-  animations:[
-    
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@listItem', [stagger(50, [animateChild()])]),
+      ]),
+    ]),
     trigger('listItem', [
-      state('default', style({
-        transform: 'scale(1)',
-        'background-color': 'white',
-        'z-index': 1
-      })),
-      state('active', style({
-        transform: 'scale(1.05)',
-        'background-color': 'rgb(201, 157, 242)',
-        'z-index': 2
-      })),
-      transition('default => active', [
-        animate('100ms ease-in-out')
-      ]),
-      transition('active => default', [
-        animate('500ms ease-in-out')
-      ]),
-      transition('void => *',[
+      state(
+        'default',
+        style({
+          transform: 'scale(1)',
+          'background-color': 'white',
+          'z-index': 1,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+          'background-color': 'rgb(201, 157, 242)',
+          'z-index': 2,
+        })
+      ),
+      transition('default => active', [animate('100ms ease-in-out')]),
+      transition('active => default', [animate('500ms ease-in-out')]),
+      transition('void => *', [
+        query(
+          '.comment-text, .comment-date',
+          style({
+            opacity: 0,
+          })
+        ),
         style({
           transform: 'translateX(-100%)',
-          opacity:0,
-          'background-color': 'rgb(201, 157, 242)'
+          opacity: 0,
+          'background-color': 'rgb(201, 157, 242)',
         }),
-        animate('250ms ease-out', style({
-          transform: 'translateX(0%)',
-          opacity:1,
-          'background-color': 'white'
-
-        }))
-      ])
-    ])
-  ]
+        animate(
+          '250ms ease-out',
+          style({
+            transform: 'translateX(0%)',
+            opacity: 1,
+            'background-color': 'white',
+          })
+        ),
+        group([
+          sequence([
+            animate(
+              '250ms',
+              style({
+                'background-color': 'rgb(201, 157, 242)',
+              })
+            ),
+            animate(
+              '250ms',
+              style({
+                'background-color': 'white',
+              })
+            ),
+          ]),
+          query('.comment-text', [
+            animate(
+              '250ms',
+              style({
+                opacity: 1,
+              })
+            ),
+          ]),
+          query('.comment-date', [
+            animate(
+              '500ms',
+              style({
+                opacity: 1,
+              })
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class CommentsComponent implements OnInit {
-
   @Input() comments!: Comment[];
   @Output() newComment = new EventEmitter<string>();
 
-  commentCtrl!:FormControl;
+  commentCtrl!: FormControl;
 
-  animationState: {[key:number] : 'default' | 'active'} = {};
+  animationState: { [key: number]: 'default' | 'active' } = {};
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-
-    this.commentCtrl = this.formBuilder.control(' ', [Validators.required, Validators.minLength(10)]) 
-    for(let index in this.comments){
+    this.commentCtrl = this.formBuilder.control(' ', [
+      Validators.required,
+      Validators.minLength(10),
+    ]);
+    for (let index in this.comments) {
       this.animationState[index] = 'default';
     }
   }
 
   onLeaveComment() {
-    if(this.commentCtrl.invalid){
+    if (this.commentCtrl.invalid) {
       return;
     }
-    const maxId = Math.max(...this.comments.map(comment=> comment.id));
+    const maxId = Math.max(...this.comments.map((comment) => comment.id));
     this.comments.unshift({
-      id:maxId + 1,
+      id: maxId + 1,
       comment: this.commentCtrl.value,
       createdDate: new Date().toISOString(),
-      userId: 1
+      userId: 1,
     });
 
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
-    }
+  }
 
-    onListItemMouseEnter(index:number){
-      this.animationState[index]='active';
-    }
-    onListItemMouseLeave(index:number){
-      this.animationState[index] ='default';
-    }
+  onListItemMouseEnter(index: number) {
+    this.animationState[index] = 'active';
+  }
+  onListItemMouseLeave(index: number) {
+    this.animationState[index] = 'default';
+  }
 }
