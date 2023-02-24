9a0ced6de02c3e77bc5b35412abfce3410e2a63d
Souliman <souleymane.sada@gmail.com>
2023-02-24 13:51:27 +0000

    WIP preparing the module

---
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-list',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CandidateListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/reactive-state/services/candidates.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Candidate } from '../models/candidate.model';
+
+@Injectable()
+export class CandidatesServices {
+  constructor(private http: HttpClient) {}
+  private _loading$ = new BehaviorSubject<boolean>(false);
+
+  get loading$(): Observable<boolean> {
+    return this._loading$.asObservable();
+  }
+
+  private _candidate$ = new BehaviorSubject<Candidate[]>([]);
+
+  get candidate$(): Observable<Candidate[]> {
+    return this._candidate$.asObservable();
+  }
+}
